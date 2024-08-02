e81e88bca14b73531ae62275e8c523620a34b250
vibhuti verma <jenny.vibhuti@gmail.com>
2024-08-02 20:18:02 +0530

    commit change

--- app.py
+++ app.py
@@ -32,21 +32,13 @@ def read_sql_query(sql,db):
 ## Define your Prompt
 prompt = [
     """
-    You are an expert in converting English questions to SQL query! 
-    The SQL database has two tables \nThe table 1 STUDENT and has the following columns - NAME, CLASS, 
-    SECTION and MARKS and another table 2 PROFILE and has the following columns - NAME, SEX AND REGION
-    \n\nFor example,\nExample 1 - How many entries of records are present?, 
+  You are an expert in converting English questions to SQL query!
+    The SQL database has the name STUDENT and has the following columns - NAME, CLASS, 
+    SECTION \n\nFor example,\nExample 1 - How many entries of records are present?, 
     the SQL command will be something like this SELECT COUNT(*) FROM STUDENT ;
     \nExample 2 - Tell me all the students studying in Data Science class?, 
     the SQL command will be something like this SELECT * FROM STUDENT 
-    where CLASS="Data Science";
-    \n\nFor example,\nExample 3 - How many entries of records are present?, 
-    the SQL command will be something like this SELECT COUNT(*) FROM PROFILE ;
-    \nExample 4 - Tell me all the students from Pune?, 
-    the SQL command will be something like this SELECT * FROM PROFILE 
-    where REGION="Pune"; 
-    \nExample 5 - Give me name,marks,section,class and are from Pune?, 
-    the SQL command will be something like this SELECT NAME,MARKS,SECTION,CLASS,REGION FROM PROFILE INNER JOIN STUDENT ON PROFILE.NAME = STUDENT.NAME where REGION="Pune"; 
+    where CLASS="Data Science"; 
     also the sql code should not have ``` in beginning or end and sql word in output
 
     """
--- sql.py
+++ sql.py
@@ -6,28 +6,28 @@ connection = sqlite3.connect("student.db")
 
 ## Create a cursor object to insert record,create table,retrieve
 cursor = connection.cursor()
-## create table 
-# table_info = """
-# CREATE TABLE STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),
-# SECTION VARCHAR(25),MARKS INT);
-# """
+# create table 
+table_info = """
+CREATE TABLE STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),
+SECTION VARCHAR(25),MARKS INT);
+"""
 
-##cursor.execute(table_info)
+cursor.execute(table_info)
 
-## Insert records
+#Insert records
 
-# # cursor.execute('''INSERT INTO STUDENT VALUES ('Krish', 'Data Science', 'A','50')''') 
-# # cursor.execute('''INSERT INTO STUDENT VALUES ('Darius', 'Data Science', 'B','100')''') 
-# # cursor.execute('''INSERT INTO STUDENT VALUES ('Sudhanshu', 'Devops', 'C','20')''') 
-# # cursor.execute('''INSERT INTO STUDENT VALUES ('Vikash', 'Data Science', 'C','85')''')
+cursor.execute('''INSERT INTO STUDENT VALUES ('Krish', 'Data Science', 'A','50')''') 
+cursor.execute('''INSERT INTO STUDENT VALUES ('Darius', 'Data Science', 'B','100')''') 
+cursor.execute('''INSERT INTO STUDENT VALUES ('Sudhanshu', 'Devops', 'C','20')''') 
+cursor.execute('''INSERT INTO STUDENT VALUES ('Vikash', 'Data Science', 'C','85')''')
 
-# ## Display all the records
-# print("The inserted records are ")
+## Display all the records
+print("The inserted records are ")
 
-# data = cursor.execute("SELECT * FROM STUDENT")
+data = cursor.execute("SELECT * FROM STUDENT")
 
-# for row in data:
-#     print(row)
+for row in data:
+     print(row)
 
 table_profile_info = """CREATE TABLE PROFILE(NAME VARCHAR(25),SEX VARCHAR(25),REGION VARCHAR(25));"""
 cursor.execute(table_profile_info)
